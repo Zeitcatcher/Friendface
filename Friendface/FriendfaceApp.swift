477089a9b1b80461da443438e3ffde343d4fec15
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-07-08 17:56:16 -1000

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Arseniy Oksenoyt on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
